ebb1cc4af6caf7b164a4b964892641e958fe8e0c
vpsingh404 <singhvijay82@gmail.com>
2015-10-19 20:02:39 +0530

    Profile project added with all files

---
+++ public/controller/mainCtrl.js
@@ -0,0 +1,43 @@
+
+var myApp = angular.module('profile', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
+
+	$scope.title= "vijay";
+	console.log('hello vj');
+
+	var refresh = function(){
+		$http.get('/contactlist').success(function(response){
+			$scope.contactlist = response;
+			$scope.contact = "";
+		});
+	};
+	refresh();
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.edit = function(id){
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function(response){
+			$scope.contact = response;
+		});
+	};
+
+	$scope.update = function(){
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+		});
+	};
+
+}]);
